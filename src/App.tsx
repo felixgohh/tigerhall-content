831a8503a85345063fa6112e4b34eff0861f4f6e
Felix <felixgohh08@gmail.com>
2024-06-20 00:04:47 +0700

    update style value

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Content from './components/Content';
 
 function App() {
   return (
-    <div className="w-[70%] py-[24px] mx-auto">
+    <div className="w-[70%] py-6 mx-auto">
       <header>
-        <h1 className="text-2xl font-bold text-center mb-[14px]">
+        <h1 className="text-2xl font-bold text-center mb-4">
           TigerHall Content
         </h1>
       </header>
--- src/components/ContentCards.tsx
+++ src/components/ContentCards.tsx
@@ -17,20 +17,20 @@ const ContentCards: React.FC<ContentCardsProps> = ({ cards }) => {
       {cards.map((card) => (
         <article
           key={card.name}
-          className="w-full h-full flex flex-col rounded-[10px] bg-white"
+          className="w-full h-full flex flex-col rounded-xl bg-white"
         >
           <div className="relative border-b-2 border-b-grey-400">
-            <div className="flex flex-row items-center gap-[5px] absolute left-0 top-0 bg-orange-50 p-[8px] rounded-br-[10px] rounded-tl-[10px]">
+            <div className="flex flex-row items-center gap-[5px] absolute left-0 top-0 bg-orange-50 p-2 rounded-br-[10px] rounded-tl-[10px]">
               <IonIcon
                 name="pie-chart"
                 className="text-orange-400 text-sm"
               ></IonIcon>
               <p className="text-sm font-bold text-black">0% Completed</p>
             </div>
-            <div className="flex w-[24px] h-[24px] bg-orange-600 p-[5px] rounded-full absolute left-[5px] bottom-[5px]">
+            <div className="flex w-6 h-6 bg-orange-600 p-[5px] rounded-full absolute left-[5px] bottom-[5px]">
               <IonIcon name="headset" className="text-white"></IonIcon>
             </div>
-            <div className="flex flex-row items-center gap-[5px] text-sm absolute right-[5px] bottom-[5px] py-[4px] px-[8px] rounded-[100px] bg-black text-white font-bold">
+            <div className="flex flex-row items-center gap-[5px] text-sm absolute right-[5px] bottom-[5px] py-[4px] px-2 rounded-[100px] bg-black text-white font-bold">
               <IonIcon name="time-outline"></IonIcon>
               <p>{convertMinutesToHours(card.length)}</p>
             </div>
@@ -49,7 +49,7 @@ const ContentCards: React.FC<ContentCardsProps> = ({ cards }) => {
             <h3 className="text-md font-bold mt-[3px] text-black capitalize leading-tight">
               {card.name}
             </h3>
-            <div className="text-sm capitalize leading-tight mt-[8px]">
+            <div className="text-sm capitalize leading-tight mt-2">
               <p className="text-grey-800">
                 {card.experts[0].firstName} {card.experts[0].lastName}
               </p>
