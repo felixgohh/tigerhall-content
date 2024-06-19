36c78ebae6cd48ff590891b0c67fb8bb102465ca
Felix <felixgohh08@gmail.com>
2024-06-19 20:07:49 +0700

    handle onsubmit form

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import IonIcon from '@reacticons/ionicons';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 
 interface SearchBarProps {
   searchTerm: string;
@@ -9,19 +9,25 @@ interface SearchBarProps {
 const SearchBar: React.FC<SearchBarProps> = ({
   searchTerm,
   onSearchChange,
-}) => (
-  <form role="search" autoComplete="off">
-    <div className="flex flex-row items-center p-[14px] bg-grey-900 rounded-[4px] border border-grey-700">
-      <IonIcon name="search" className="mr-[10px]"></IonIcon>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={onSearchChange}
-        aria-label="Search podcasts"
-        className="bg-transparent outline-none"
-      />
-    </div>
-  </form>
-);
+}) => {
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+  };
+
+  return (
+    <form role="search" autoComplete="off" onSubmit={handleSubmit}>
+      <div className="flex flex-row items-center p-[14px] bg-grey-900 rounded-[4px] border border-grey-700">
+        <IonIcon name="search" className="mr-[10px]"></IonIcon>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={onSearchChange}
+          aria-label="Search podcasts"
+          className="bg-transparent outline-none"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default SearchBar;
