020341c365b47930b846834d658ad32876861299
Felix <42145412+felixgohh@users.noreply.github.com>
2024-06-19 17:11:16 +0700

    feat(content): add fetch content cards flow (#2)
    * add GET_CONTENT_CARDS query
    
    * add content types
    
    * add get content cards flow

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+export const GET_CONTENT_CARDS = gql`
+  query GetContentCards($keywords: String!) {
+    contentCards(filter: { limit: 20, keywords: $keywords, types: [PODCAST] }) {
+      edges {
+        ... on Podcast {
+          name
+          image {
+            ...Image
+          }
+          categories {
+            ...Category
+          }
+          experts {
+            ...Expert
+          }
+        }
+      }
+    }
+  }
+
+  fragment Image on Image {
+    uri
+  }
+
+  fragment Category on Category {
+    name
+  }
+
+  fragment Expert on Expert {
+    firstName
+    lastName
+    title
+    company
+  }
+`;
